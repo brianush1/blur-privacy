de19e53ffba4d2dd7a6c3653104f8408ab56aacf
brianush1 <brianush1@outlook.com>
2021-02-23 15:50:56 -0500

    make userscript

---
+++ README.md
@@ -0,0 +1,1 @@
+[Install](https://raw.github.com/brianush1/blur-privacy/master/privacy.user.js)
--- privacy.user.js
+++ privacy.user.js
@@ -1,3 +1,23 @@
+// ==UserScript==
+// @name            Blur privacy
+// @description     Prevents websites from knowing when they lose your attention
+//
+// @author          Brian <brianush1@outlook.com>
+// @namespace       http://github.com/brianush1
+// @downloadURL     https://raw.github.com/brianush1/blur-privacy/master/privacy.user.js
+//
+// @license         GPLv3 - http://www.gnu.org/licenses/gpl-3.0.txt
+// @copyright       Copyright (C) 2021, by Brian <brianush1@outlook.com>
+//
+// @include         *://*/*
+// @match           *://*/*
+//
+// @version         1.0
+// @updateURL       https://raw.github.com/brianush1/blur-privacy/master/privacy.user.js
+//
+// @run-at          document-start
+// ==/UserScript==
+
 (() => {
 	if (document.readyState !== "loading") {
 		return;
